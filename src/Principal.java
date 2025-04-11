c769c0fdc153d18e387857080cd19d116fa65619
Luisa <luisafranconeves@gmail.com>
2025-04-11 12:00:07 -0300

    criando objetos

---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O Poderoso Chefão";
+        meuFilme.anoDeLancamento = 1972;
+        meuFilme.duracaoEmMinutos = 153;
+        System.out.println(meuFilme.nome);
+        System.out.println(meuFilme.anoDeLancamento);
+    }
+}
