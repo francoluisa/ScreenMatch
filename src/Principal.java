99d7b6e39422c315bc81245c2793b02527928d74
Luisa <luisafranconeves@gmail.com>
2025-04-14 16:08:26 -0300

    metodos e modificadores de acesso

--- src/Principal.java
+++ src/Principal.java
@@ -4,7 +4,16 @@ public class Principal {
         meuFilme.nome = "O Poderoso Chefão";
         meuFilme.anoDeLancamento = 1972;
         meuFilme.duracaoEmMinutos = 153;
+        meuFilme.incluidoNoPlano = true;
 
         meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.getSomaDasAvaliacoes());
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.mediaDasAvaliacoes());
+
+
     }
 }
